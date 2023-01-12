d46b7ce9e95935df21414512759f59adb0105f70
Lucas_SPITZ <lucas.spitz@etu.univ-lyon1.Fr>
2023-01-12 11:12:08 +0100

    Question 1 - Création squelette Classe Personne

---
+++ TD_Bibliotheque/src/td_bibliotheque/Personne.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package td_bibliotheque;
+
+/**
+ *
+ * @author p2200327
+ */
+public class Personne {
+    
+    private int numeroPers, anNaissance;
+    private String nomPers, prenomPers;
+    private static int dernierNumero;
+    
+    Personne(String nom, String prenom, int anneeNaissance){
+        
+    }
+    
+    //SETTERS
+    public void setNumPers(int numero){
+        
+    }
+    
+    public void setNomPers(String nom){
+        
+    }
+    
+    public void setPrenomPers(String prenom){
+        
+    }
+    
+    public void setAnNaissance(int annee){
+        
+    }
+    
+    //GETTERS
+    public int getDernierNum(){
+        
+        return 0;
+    }
+    
+    public int getNumero(){
+        
+        return 1;
+    }
+    
+    public String getNom(){
+        
+        return nomPers;
+    }
+    
+    public String getPrenom(){
+        
+        return prenomPers;
+    }
+    
+    public int getAnNaissance(){
+        
+        return -1;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return "rien";
+    }
+   
+}
