9a7d9b8ac7955dcbfd7b8d8e0a1de0fefc3a1035
Lucas_SPITZ <lucas.spitz@etu.univ-lyon1.Fr>
2023-01-12 11:38:13 +0100

    Refactoring TestPersonne et refactoring nom du package

--- TD_Bibliotheque/test/Test_Personne.java
+++ TD_Bibliotheque/test/Test_Personne.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package td_bibliotheque;
 
+import bibliotheque.Personne;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
