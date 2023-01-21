8085de79367673211d37afe55d425ab7d32d0815
Yodwaae <lucas.spitz.drevet@gmail.com>
2023-01-21 15:56:36 +0100

    Première partie de la question 4
    NB : Yodwaae est le compte Gitlab personnel de Lucas SPITZ

--- TD_Bibliotheque/test/Test_Personne.java
+++ TD_Bibliotheque/test/Test_Personne.java
@@ -12,11 +12,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
 
+
+
+
 /**
  *Réponse Question 3 du TP:
  * 1- Lors de l'ajout de @Ignore, NetBeans nous propose l'import de org.junit.Ignore.
- * 2- Cela permet de skip le test et de ne pas retourner d'erreurs mais cela ne valide pas le test global à 100% pour autant.
+ * 2- Cela permet de skip le test et de ne pas retourner d'erreurs mais cela ne 
+ * valide pas le test global à 100% pour autant.
 */
+
+/**
+ * Réponse Question 4 du TP :
+ * 1-Les différentes sont : utiliser un compteur statique pour set le numéro de 
+ * personne à sa création puis incrémenter le compteur OU utiliser AtomicInteger
+ * 2- L'avantage du compteur statique est que Personne ne dépend pas d'une autre
+ * classe, l'inconvénient est que si on veut ajouter un numéro à la classe Livre
+ * aussi on va dupliquer du code. L'avantage d'AtomicInteger est qu'il évite la 
+ * duplication de code si on veut ajouter aussi un numéro à la Classe Livre. 
+ * L'inconvénient est qu'il y a maintenant un couplage entre nos classes et 
+ * AtomicInteger (toutefois AtomicInteger est stable).
+ * 
+ */
+
 /**
  *
  * @author audranmalosse
