df832a905a0df2c3ca71becdffc413dad6aa8cee
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2023-01-24 07:58:37 +0100

    Commentaire réponse Question 4 (qui avait été supprimé)

--- TD_Bibliotheque/test/Test_Personne.java
+++ TD_Bibliotheque/test/Test_Personne.java
@@ -18,14 +18,17 @@ import org.junit.Ignore;
  * 2- Cela permet de skip le test et de ne pas retourner d'erreurs mais cela ne 
  * valide pas le test global à 100% pour autant.
  * 
- * Réponse Question 4 du TP:
- * On peut les rentrer manuelement via un argument transmis au constructeur ou 
- * le faire via un compteur
+ * Réponse Question 4 du TP :
+ * 1-Les différentes sont : utiliser un compteur statique pour set le numéro de 
+ * personne à sa création puis incrémenter le compteur OU utiliser AtomicInteger
+ * 2- L'avantage du compteur statique est que Personne ne dépend pas d'une autre
+ * classe, l'inconvénient est que si on veut ajouter un numéro à la classe Livre
+ * aussi on va dupliquer du code. L'avantage d'AtomicInteger est qu'il évite la 
+ * duplication de code si on veut ajouter aussi un numéro à la Classe Livre. 
+ * L'inconvénient est qu'il y a maintenant un couplage entre nos classes et 
+ * AtomicInteger (toutefois AtomicInteger est stable).
  * 
- * avantage/inconvénient: 
- * compteur : Les numéros sont uniques/On ne choisit pas le numéro d'une personne
- * atomic integer : Les numeros sont.....
-*/
+ */
 /**
  *
  * @author audranmalosse
